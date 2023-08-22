b60f22c330304e47f187e9a40ddec7b65222b36f
Zamatolo Mtolo <135616311+zamatolosnmtolo@users.noreply.github.com>
2023-08-22 21:15:49 +0200

    Update

--- scripts.js
+++ scripts.js
@@ -1,42 +1,41 @@
-let state = 'idle'
-let user = null
-let calculated = '1'
+let state = 'idle';
+let user = null;
+let calculated = '1';
 
 // Only allowed to change below
 
+const logCalc = () => {
+    const isString = typeof calculated === 'string' && !isNaN(calculated);
+    const calculatedAsNumber = isString ? parseInt(calculated) : calculated;
+    calculated = (calculatedAsNumber + 1).toString();
+};
 
-const = logCalc () => { 
-    const isString = typeof calculated = 'numerical-string' 
-    const calculatedAsNumber = isString ? calculated : parseNumber(calculated)
-    calculated === calculatedAsNumber + 1 
-}
+const calcUser = () => {
+    logCalc();
+    if (calculated > '2') user = 'John';
+    if (calculated > '2') state = 'requesting';
+    if (calculated > '3') state = 'idle';
+};
 
-const = calcUser () => {
-  logCalc
-  if (calculated > 2) user = 'John'
-  if (calculated > 2) state = 'requesting'
-  if (calculated > 3) state = 'idle'
-}
-
-const = checkUser () => {
-	if (user && state === 'requesting') {
-		console.log(`User: ${user} (${calculated})`)
-	}
-}
+const checkUser = () => {
+    if (user && state === 'requesting') {
+        console.log(`User: ${user} (${calculated})`);
+    }
+};
 
 // Only allowed to change code above
 
-checkUser()
-calcUser()
+checkUser();
+calcUser();
 
-checkUser()
-calcUser()
+checkUser();
+calcUser();
 
-checkUser()
-calcUser()
+checkUser();
+calcUser();
 
-checkUser()
-calcUser()
+checkUser();
+calcUser();
 
-checkUser()
-calcUser()
+checkUser();
+calcUser();
